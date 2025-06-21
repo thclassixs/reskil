5bcb180f8ba576b890e214590a458e9a660b49ef
thclassixs <thclassixs@gmail.com>
2025-06-21 17:44:13 +0100

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -160,7 +160,8 @@ export default function LandingPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
-              <a href="/" className="flex items-center">ResKil
+              <a href="/" className="text-2xl font-bold text-blue-600 flex items-center">
+                ResKil
               </a>
             </div>
             <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
