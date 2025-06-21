2b4d52827efdf79cace452f0b1469d67c5648b03
thclassixs <thclassixs@gmail.com>
2025-06-21 17:42:17 +0100

    reskil

--- app/page.tsx
+++ app/page.tsx
@@ -160,12 +160,7 @@ export default function LandingPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
-              <a href="/" className="flex items-center">
-              <img 
-                  src="/REL.png"
-                  alt="ResKil" 
-                  className="h-8 w-auto" // Adjust height as needed
-                />
+              <a href="/" className="flex items-center">ResKil
               </a>
             </div>
             <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
